a30f214b062d1e63762dc64b5e2bf0522500621d
maxwellkemosi076-lab <maxwellkemosi076@gmail.com>
2025-09-24 18:56:10 +0300

    first commit

---
+++ Display_units.c
@@ -0,0 +1,20 @@
+/*
+Name: Maxwell Mokogi.
+REG number: PA106/G/28786/25.
+Description: Displaying units.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("1. Discrete mathematics \n");
+    printf("2. Mathematics for science \n");
+    printf("3. Fundamentals of ICT \n");
+    printf("4. Computer Systems and Organisation \n");
+    printf("5. Communication skills \n");
+    printf("6. Introduction to programming and problem solving \n");
+    printf("7. Fundamental of system analysis and design \n");
+
+    return 0;
+}
